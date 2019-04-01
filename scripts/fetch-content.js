c15d55ea7fecb89f27c3513759ab7cf6b8cc7a55
Marco Cedaro <cedmax@users.noreply.github.com>
2019-04-01 21:42:41 +0100

    build per language

--- scripts/fetch-content.js
+++ scripts/fetch-content.js
@@ -1,67 +1,65 @@
 /* eslint-disable no-restricted-syntax, no-await-in-loop */
 
-require('dotenv').config()
-const download = require('image-downloader')
-const contentfulStatic = require('contentful-static')
-const save = require('./structure-content')
+require("dotenv").config();
+const download = require("image-downloader");
+const contentfulStatic = require("contentful-static");
+const save = require("./structure-content");
 
 const destructureImg = image => {
-  const { file: { url, fileName } } = image
-  return { url, fileName }
-}
+  const {
+    file: { url, fileName },
+  } = image;
+  return { url, fileName };
+};
 
-async function saveImages (content) {
+async function saveImages(content) {
   for (const item of content) {
-    const data = destructureImg(item.image)
-  
+    const data = destructureImg(item.image);
+
     const options = {
       url: `http:${data.url}`,
-      dest: `./templates/images/shows/${data.fileName}`
-    }
+      dest: `./templates/images/shows/${data.fileName}`,
+    };
 
-    await download.image(options)
+    await download.image(options);
   }
 }
 
-function sync (space, accessToken) {
+function sync(space, accessToken, lang) {
   contentfulStatic.config({
     space,
-    accessToken
-  })
+    accessToken,
+  });
 
   contentfulStatic.sync(async (err, json) => {
     if (err) {
-      console.log('contentful-static: data could not be fetched')
-      return false
+      console.log("contentful-static: data could not be fetched");
+      return false;
     }
 
-    const languages = Object.keys(json.entries)
+    const content = json.entries[lang];
 
-    for (const lang of languages) {
-      const content = json.entries[lang]
-      const shows = content
-        .filter(item => item.sys.contentType.sys.id === 'shows')
-        .map(item => {
-          item.fields.image = item.fields.image.fields
-          return item.fields
-        })
-      const metadata = content
-        .filter(item => item.sys.contentType.sys.id === 'metadata')
-        .map(item => item.fields)
+    const shows = content
+      .filter(item => item.sys.contentType.sys.id === "shows")
+      .map(item => {
+        item.fields.image = item.fields.image.fields;
+        return item.fields;
+      });
+    const metadata = content
+      .filter(item => item.sys.contentType.sys.id === "metadata")
+      .map(item => item.fields);
 
-      save(
-        {
-          shows,
-          metadata
-        }
-      )
+    save({
+      shows,
+      metadata,
+    });
 
-      await saveImages(shows)
-    }
-  })
+    await saveImages(shows);
+  });
 }
 
 sync(
   process.env.CONTENTFUL_SPACE,
-  process.env.CONTENTFUL_KEY
-)
+  process.env.CONTENTFUL_KEY,
+  process.env.LANGUAGE
+);
